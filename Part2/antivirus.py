a5acf4587a6c378e537e3cf785e35a332ab6a6aa
nGardin <gardin.nic@gmail.com>
2018-11-08 17:52:28 -0500

    Implemented quarantine

--- Part2/antivirus.py
+++ Part2/antivirus.py
@@ -1,3 +1,4 @@
+import os
 class Anti_virus(object):
 	
 	'''this is basically the UI, prompt the user for a virus
@@ -19,15 +20,33 @@ class Anti_virus(object):
 	'''read the file and run _convert_to_offset then compare
 	the output with self.offset_defs.
 
-	@returns a tupple of filename and the integer offset of 
+	@returns a tuple of filename and the integer offset of 
 	the start of infected sequence'''
 	def check_file(self, filename):
+
+
 		pass
+
 
 	'''set the first 8 bytes of the sequence to "xxxxxxxx", then
 	move the file to some directory'''
 	def nullify_and_quarantine(self, filename, first_byte_index):
+
+		#print(os.listdir())
+		with open(filename, 'r') as myFile:
+			data = myFile.read()
+
+		#print(data)
+		dataList = list(data)
+		for i in range(first_byte_index, first_byte_index+8):
+			dataList[i] = "x"
+
+		newData = ''.join(dataList);
+		#print(newData)
+
+
 		pass
+
 
 	'''convert an array of bytes into an array of integer offsets.
 	The offsets should be the first byte minus the next byte.
@@ -36,3 +55,15 @@ class Anti_virus(object):
 	@returns a list of integer offsets'''
 	def _convert_to_offset(self, bytes):
 		pass
+
+
+
+
+
+def main():
+	test = Anti_virus()
+	test.nullify_and_quarantine("VirusDefinitionFile.txt", 10)
+
+
+if __name__ == '__main__':
+	main()
